2d60b76be04b1235a6769b37d52d3096262823a6
Henrique Gomes <henriquegomes@outlook.com>
2025-10-16 16:00:07 -0300

    refactor: remove unused files

--- src/components/Header/index.jsx
+++
@@ -1,7 +0,0 @@
-import { HeaderContainer } from "./style"
-
-export const Header = ({ children }) => {
-    return(
-        <HeaderContainer>{ children }</HeaderContainer>
-    )
-}
--- src/components/Section/index.jsx
+++
@@ -1,5 +0,0 @@
-import { SectionStyled } from "./style";
-
-export const Section = ({ children }) => {
-    return (<SectionStyled>{ children }</SectionStyled>)
-}
