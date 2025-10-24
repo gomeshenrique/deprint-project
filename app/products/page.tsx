a04dd8a6a2b87d7d7318d6162eb8196be13f78e9
Henrique Gomes <henriquegomes@outlook.com>
2025-10-24 10:55:06 -0300

    feat: creating dynamic routes for products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,27 +1,66 @@
-export default function Produts() {
-  const Products = [
-    { id: 1, name: "Product 1", description: "Description for Product 1" },
-    { id: 2, name: "Product 2", description: "Description for Product 2" },
-    { id: 3, name: "Product 3", description: "Description for Product 3" },
-    { id: 4, name: "Product 4", description: "Description for Product 4" },
-    { id: 5, name: "Product 5", description: "Description for Product 5" },
-    { id: 6, name: "Product 6", description: "Description for Product 6" },
-  ];
+import Link from "next/link";
+
+export type Product = {
+  id: number;
+  documentId: string;
+  Title: string;
+  Description: string;
+  Images: Image[];
+};
+
+type Image = {
+  id: number;
+  documentId: string;
+  alternativeText: string;
+  ext: string;
+  mime: string;
+  url: string;
+};
+
+async function getProdutcs(): Promise<Product[]> {
+  let responseJson;
+
+  try {
+    const response = await fetch(
+      `${process.env.STRAPI_BASE_URL}/api/products`,
+      {
+        headers: {
+          Authorization: `bearer ${process.env.STRAPI_KEY}`,
+        },
+      }
+    );
+
+    responseJson = await response.json();
+  } catch (error) {
+    console.error(`Erro ao carregar lista de produtos: ${error}`);
+  }
+
+  return responseJson.data;
+}
+
+export default async function Produts() {
+  const products = await getProdutcs();
 
   return (
     <main className="container mt-32 mb-8 mx-auto px-4 lg:px-8">
       <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold mb-6 text-center text-balance">
         Nossos produtos
       </h1>
-      {Products.length ? (
+      {products.length ? (
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12">
-          {Products.map((product) => (
+          {products.map((product) => (
             <div
               key={product.id}
               className="p-4 border border-border rounded-md light:shadow-md w-full"
             >
-              <h2>{product.name}</h2>
-              <p>{product.description}</p>
+              <h2>{product.Title}</h2>
+              <p>{product.Description}</p>
+              <Link
+                className="mt-4 px-4 py-2 bg-primary text-white rounded-md hover:bg-primary-dark"
+                href={`/products/${product.documentId}`}
+              >
+                Ver detalhes
+              </Link>
             </div>
           ))}
         </div>
